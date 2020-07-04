4d3683f58eb38501e9cfd703fb2dca2178a70a38
M-Elsaeed <m.elsaeed1998@gmail.com>
2020-07-04 12:24:04 +0200

    Refactoring

--- water_heater.X/water_heater_main.c
+++ water_heater.X/water_heater_main.c
@@ -7,8 +7,8 @@
 #include "adc.h"
 
 // Heater and cooler state variables
-unsigned char heater_on = 0;
-unsigned char cooler_on = 0;
+unsigned char gb_heater_on = 0;
+unsigned char gb_cooler_on = 0;
 // Defining pins for heating and cooling elements and the indicator LED
 #define HEATING_COOLING_LED RB6
 #define HEATING_ELEMENT_PIN RC5
@@ -18,7 +18,7 @@ unsigned char cooler_on = 0;
 #define ON_STATE 'O'
 #define OFF_STATE 'F'
 #define SETTING_STATE 'S'
-unsigned char state = OFF_STATE;
+unsigned char g_state = OFF_STATE;
 
 // Mask to turn on 7SDs
 #define ON_7SD 0xff
@@ -32,31 +32,31 @@ Down -> R1
 #define DOWN_BTN RB1
 
 // To track number of blinks when setting the temperature
-unsigned char n_blinks = 0;
+unsigned char g_num_of_blinks = 0;
 
 // Avg of last 10 measured temperatures, initially equals avg of max and min possible set temperatures
-unsigned int avg_measured_temperature = 55;
+unsigned int g_avg_measured_temperature = 55;
 
 // The temperature set by the user, loaded from external E2PROM on startup if available, or default(60)
-unsigned int set_temperature = 60;
+unsigned int g_set_temperature = 60;
 
 // Counter to measure 1 sec, incremented by 1 every 100ms in ISR of timer 1, and is reset when it reaches 10
-unsigned char t_1sec = 0;
+unsigned char g_tenths_of_1sec = 0;
 
-// Mask for blinking LED in the setting state
-unsigned char _7sd_mask = ON_7SD;
+// Mask for blinking LED in the setting g_state
+unsigned char g_7sd_mask = ON_7SD;
 
-// Toggled every 10ms to track which 7SD to output to at some given time
-unsigned char i_7sd = 0;
+// Updated every 10ms to track which 7SD to output to at some given time
+unsigned char g_7sd_index = 0;
 
 // Array to track last 10 temperature measurements
-unsigned int temps[10];
+unsigned int g_last_ten_temps[10];
 
 /* 
- iterator to track the index of the measurement to update in the "temps" array next
+ iterator to track the index of the measurement to update in the "g_last_ten_temps" array next
  Reset when it reaches 10
 */
-int temps_iterator = 0;
+int g_temps_arr_iterator = 0;
 
 // Function Declarations
 
@@ -78,10 +78,10 @@ void main();
 // Activate heating element, its LED, and set the heating state variable
 void activate_heater()
 {
-  if (!heater_on)
+  if (!gb_heater_on)
   {
     HEATING_ELEMENT_PIN = 1;
-    heater_on = 1;
+    gb_heater_on = 1;
     HEATING_COOLING_LED = 1;
   }
 }
@@ -89,11 +89,11 @@ void activate_heater()
 // Activate heating element, its LED, and clear the heating state variable
 void deactivate_heater()
 {
-  if (state == OFF_STATE || set_temperature == avg_measured_temperature || cooler_on)
+  if (g_state == OFF_STATE || g_set_temperature == g_avg_measured_temperature || gb_cooler_on)
   {
     HEATING_ELEMENT_PIN = 0;
-    heater_on = 0;
-    if (!cooler_on)
+    gb_heater_on = 0;
+    if (!gb_cooler_on)
       HEATING_COOLING_LED = 0;
   }
 }
@@ -101,10 +101,10 @@ void deactivate_heater()
 // Activate cooling element, its LED, and set the cooling state variable
 void activate_cooler()
 {
-  if (!cooler_on)
+  if (!gb_cooler_on)
   {
     COOLING_ELEMENT_PIN = 1;
-    cooler_on = 1;
+    gb_cooler_on = 1;
     HEATING_COOLING_LED = 1;
   }
 }
@@ -112,56 +112,56 @@ void activate_cooler()
 // Dectivate cooling element, its LED, and clear the cooling state variable
 void deactivate_cooler()
 {
-  if (state == OFF_STATE || set_temperature == avg_measured_temperature || heater_on)
+  if (g_state == OFF_STATE || g_set_temperature == g_avg_measured_temperature || gb_heater_on)
   {
     COOLING_ELEMENT_PIN = 0;
-    cooler_on = 0;
-    if (!heater_on)
+    gb_cooler_on = 0;
+    if (!gb_heater_on)
       HEATING_COOLING_LED = 0;
   }
 }
 
-// Calculates the average of values in the "temps" array and returns it.
+// Calculates the average of values in the "g_last_ten_temps" array and returns it.
 unsigned int calc_avg()
 {
   char i;
   unsigned int avg = 0;
   for (i = 0; i < 10; i++)
   {
-    avg += temps[i];
+    avg += g_last_ten_temps[i];
   }
   return avg / 10;
 }
 
 /*
 
-Writes to 7SD then updates i_7sd to write to the next 7sd when called the next time.
+Writes to 7SD then updates g_7sd_index to write to the next 7sd when called the next time.
 
 */
 void show_7sd()
 {
-  switch (i_7sd)
+  switch (g_7sd_index)
   {
   case 0:
     PORTA = 0x20;
-    PORTD = _7sd_mask & (display7s(12));
-    i_7sd = 1;
+    PORTD = g_7sd_mask & (display7s(12));
+    g_7sd_index = 1;
     break;
   case 1:
     PORTA = 0x10;
-    if (state == SETTING_STATE)
-      PORTD = _7sd_mask & (display7s((unsigned char)(set_temperature % 10)));
+    if (g_state == SETTING_STATE)
+      PORTD = g_7sd_mask & (display7s((unsigned char)(g_set_temperature % 10)));
     else
-      PORTD = _7sd_mask & (display7s((unsigned char)(avg_measured_temperature % 10)));
-    i_7sd = 2;
+      PORTD = g_7sd_mask & (display7s((unsigned char)(g_avg_measured_temperature % 10)));
+    g_7sd_index = 2;
     break;
   case 2:
     PORTA = 0x08;
-    if (state == SETTING_STATE)
-      PORTD = _7sd_mask & (display7s((unsigned char)(set_temperature / 10)));
+    if (g_state == SETTING_STATE)
+      PORTD = g_7sd_mask & (display7s((unsigned char)(g_set_temperature / 10)));
     else
-      PORTD = _7sd_mask & (display7s((unsigned char)(avg_measured_temperature / 10)));
-    i_7sd = 0;
+      PORTD = g_7sd_mask & (display7s((unsigned char)(g_avg_measured_temperature / 10)));
+    g_7sd_index = 0;
     break;
   default:
     break;
@@ -175,33 +175,33 @@ void show_7sd()
 */
 void _1s_handler()
 {
-  if (heater_on)
+  if (gb_heater_on)
   {
     PORTB ^= (1 << 6); // Toggle RB6
   }
-  if (state == SETTING_STATE)
+  if (g_state == SETTING_STATE)
   {
-    _7sd_mask = (_7sd_mask == ON_7SD) ? 0x00 : ON_7SD;
-    if ((++n_blinks) == 10)
+    g_7sd_mask = (g_7sd_mask == ON_7SD) ? 0x00 : ON_7SD;
+    if ((++g_num_of_blinks) == 10)
     {
-      e2pext_w(10, set_temperature);
-      n_blinks = 0;
-      state = ON_STATE;
+      e2pext_w(10, g_set_temperature);
+      g_num_of_blinks = 0;
+      g_state = ON_STATE;
     }
   }
 }
 
 /* 
 1 - Measures the temperature using the ADC
-2 - updates "temps" array and its iterator
-3 - updates "avg_measured_temperature" with the new average of the last 10 temperatures
+2 - updates "g_last_ten_temps" array and its iterator
+3 - updates "g_avg_measured_temperature" with the new average of the last 10 temperatures
 */
 void adc_sample_and_update_average()
 {
   adc_init();
-  temps[temps_iterator++] = (adc_sample(2) * 10) / 20;
-  avg_measured_temperature = calc_avg();
-  temps_iterator = (temps_iterator > 9) ? 0 : temps_iterator;
+  g_last_ten_temps[g_temps_arr_iterator++] = (adc_sample(2) * 10) / 20;
+  g_avg_measured_temperature = calc_avg();
+  g_temps_arr_iterator = (g_temps_arr_iterator > 9) ? 0 : g_temps_arr_iterator;
 }
 
 // Enables interrupts on timer0, timer1, and RB0
@@ -255,7 +255,7 @@ ISR runs when any enabled interrupt happens
 
  3- Timer1 interrupts are used for
     3.1 - Sampling of the temperature every interrupt (100ms)
-    3.2 - Blinking of the displays every 10 interrupts (1s) in setting mode by toggeling _7sd_mask
+    3.2 - Blinking of the displays every 10 interrupts (1s) in setting mode by toggeling g_7sd_mask
     3.3 - Blinking heating element LED every 10 interrupts (1s) when heating element is on
 */
 void interrupt ISR()
@@ -263,18 +263,18 @@ void interrupt ISR()
   // RB0 was pressed
   if (INTF)
   {
-    state = state == ON_STATE || state == SETTING_STATE ? OFF_STATE : ON_STATE;
+    g_state = g_state == ON_STATE || g_state == SETTING_STATE ? OFF_STATE : ON_STATE;
     INTF = 0;
     return;
   }
 
   // If on or setting mode
-  if (state != OFF_STATE)
+  if (g_state != OFF_STATE)
   {
     // Enable LCD if on
-    if (state == ON_STATE)
+    if (g_state == ON_STATE)
     {
-      _7sd_mask = ON_7SD;
+      g_7sd_mask = ON_7SD;
     }
 
     // If timer 0
@@ -290,9 +290,9 @@ void interrupt ISR()
     if (TMR1IF)
     {
       adc_sample_and_update_average();
-      if (++t_1sec == 10)
+      if (++g_tenths_of_1sec == 10)
       {
-        t_1sec = 0;
+        g_tenths_of_1sec = 0;
         _1s_handler();
       }
       // reload, clear interrupt flag, and restart timer
@@ -321,11 +321,11 @@ void set_temp_exteeprom_check()
   read_char = e2pext_r(11);
   if (read_char == 0x33)
   {
-    set_temperature = e2pext_r(10);
+    g_set_temperature = e2pext_r(10);
   }
   else
   {
-    e2pext_w(10, set_temperature);
+    e2pext_w(10, g_set_temperature);
     e2pext_w(11, 0x33);
   }
 }
@@ -363,40 +363,40 @@ void main()
   */
   while (1)
   {
-    if (state != OFF_STATE)
+    if (g_state != OFF_STATE)
     {
       if (!DOWN_BTN)
       {
-        n_blinks = 0;
+        g_num_of_blinks = 0;
         while (!DOWN_BTN)
           ;
 
-        if (state == SETTING_STATE && set_temperature >= 40)
+        if (g_state == SETTING_STATE && g_set_temperature >= 40)
         {
           GIE = 0;
-          set_temperature -= 5;
+          g_set_temperature -= 5;
           GIE = 1;
         }
         else
-          state = SETTING_STATE;
+          g_state = SETTING_STATE;
       }
       else if (!UP_BTN)
       {
-        n_blinks = 0;
+        g_num_of_blinks = 0;
         while (!UP_BTN)
           ;
-        if (state == SETTING_STATE && set_temperature <= 70)
+        if (g_state == SETTING_STATE && g_set_temperature <= 70)
         {
           GIE = 0;
-          set_temperature += 5;
+          g_set_temperature += 5;
           GIE = 1;
         }
         else
-          state = SETTING_STATE;
+          g_state = SETTING_STATE;
       }
       // +ve diff, Heater On
       // -ve diff, Cooler On
-      difference = set_temperature - avg_measured_temperature;
+      difference = g_set_temperature - g_avg_measured_temperature;
       if (difference >= 5)
       {
         activate_heater();
